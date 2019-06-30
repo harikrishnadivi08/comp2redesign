3f795c49db219b1b7941b657436b7f52df67bc0d
harikrishnadivi08 <48080386+harikrishnadivi08@users.noreply.github.com>
2019-06-29 20:33:13 -0400

    Update main.js

--- composite 2 redesign/main.js
+++ composite 2 redesign/main.js
@@ -1,1 +1,27 @@
+var objPeople = [
+    {
+        username: "humber",
+        password: "humber01"
+    },
+    {
+        username: "hari",
+        password: "hari01"
+    },
+    {
+        username: "rocco",
+        password: "rocco01"
+    },
+]
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
 
+for (i=0; i < objPeople.length; i++) {
+if (username == objPeople[i].username && password == objPeople[i].password)
+    {
+    console.log(username +"is logged in."); 
+    return;
+    }
+}
+console.log("incorrect username or password");
+}
